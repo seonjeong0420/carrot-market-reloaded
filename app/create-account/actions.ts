204d58da65592e1f672a838c884152fad45a17c6
정선정 <seonjeong0420@sk.com>
2024-06-23 21:13:55 +0900

    feat: zod refine 기능 추가

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,12 +1,43 @@
 "use server";
 import { z } from "zod";
 
-const formSchema = z.object({
-  username: z.string().min(3).max(10),
-  email: z.string().email(),
-  password: z.string().min(10),
-  confirmPW: z.string().min(10),
-});
+// username validation check
+const checkUsername = (username: string) => {
+  // return username.includes("tomato") ? false : true;
+  return !username.includes("tomato");
+};
+
+// password, confirmPW 같은지 check
+const checkPassword = ({
+  password,
+  confirmPW,
+}: {
+  password: string;
+  confirmPW: string;
+}) => {
+  return password === confirmPW;
+};
+
+/**username을 필수값이 아닌 값으로 변경하고 싶은 경우 optionaal을 사용하면 된다. 
+  username: z.string().min(3).max(10).optional, */
+const formSchema = z
+  .object({
+    username: z
+      .string({
+        invalid_type_error: "username must be a string.",
+        required_error: "where is my username?",
+      })
+      .min(3, "way too short")
+      .max(10, "that is to loong")
+      .refine(checkUsername, "no tomato alllowed."),
+    email: z.string().email(),
+    password: z.string().min(10),
+    confirmPW: z.string().min(10),
+  })
+  .refine(checkPassword, {
+    message: "Both paswords should be the same.",
+    path: ["confirmPW"],
+  }); // object 안에 있는 데이터를 validation 체크하고 싶을 때 사용
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
