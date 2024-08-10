2bc9e61329aa0edf32c843d42e9fdf470ff0abab
정선정 <seonjeong0420@sk.com>
2024-08-10 17:19:29 +0900

    fix: Load More 버튼 클릭 시 DB에 있는 데이터를 설정한 page 값 만큼 렌더링

--- app/(tabs)/products/actions.ts
+++ app/(tabs)/products/actions.ts
@@ -1,6 +1,11 @@
 "use server";
 import db from "@/lib/db";
 
+/**
+ * 페이지가 10개씩 show 하는 기능을 구현하고 싶다면, 아래와 같이 수정하면 된다.
+ * skip : page * 10
+ * take: 10
+ */
 export async function getMoreProducts(page: number) {
   const products = await db.product.findMany({
     select: {
@@ -10,7 +15,7 @@ export async function getMoreProducts(page: number) {
       photo: true,
       id: true,
     },
-    skip: 1,
+    skip: page * 1,
     take: 1,
     orderBy: {
       created_at: "desc",
--- components/product-list.tsx
+++ components/product-list.tsx
@@ -10,11 +10,18 @@ interface ProductListProps {
 
 const ProductList = ({ initialProducts }: ProductListProps) => {
   const [products, setProducts] = useState(initialProducts);
+  const [page, setPage] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
+  const [isLastPage, setIsLastPage] = useState(false);
   const onLoadMoreClick = async () => {
     setIsLoading(true);
-    const newProducts = await getMoreProducts(1);
-    setProducts((prev) => [...prev, ...newProducts]);
+    const newProducts = await getMoreProducts(page + 1);
+    if (newProducts.length !== 0) {
+      setPage((prev) => prev + 1);
+      setProducts((prev) => [...prev, ...newProducts]);
+    } else {
+      setIsLastPage(true);
+    }
     setIsLoading(false);
   };
   return (
@@ -22,13 +29,15 @@ const ProductList = ({ initialProducts }: ProductListProps) => {
       {products.map((product) => (
         <ListProduct key={product.id} {...product} />
       ))}
-      <button
-        className="text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
-        onClick={onLoadMoreClick}
-        disabled={isLoading}
-      >
-        {isLoading ? "로딩중" : "Load More"}
-      </button>
+      {!isLastPage && (
+        <button
+          className="text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
+          onClick={onLoadMoreClick}
+          disabled={isLoading}
+        >
+          {isLoading ? "로딩중" : "Load More"}
+        </button>
+      )}
     </div>
   );
 };
