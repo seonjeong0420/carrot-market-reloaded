74c7017eb0e0ed44cfbb22058a19b0411ff27683
정선정 <seonjeong0420@sk.com>
2024-08-11 19:28:52 +0900

    feat: Modal data binding 기능 추가

--- app/(tabs)/home/@modal/(...)products/[id]/page.tsx
+++ app/(tabs)/home/@modal/(...)products/[id]/page.tsx
@@ -1,7 +1,77 @@
 import React from "react";
+import { UserIcon, XMarkIcon } from "@heroicons/react/24/solid";
+import Button from "@/components/button";
+import db from "@/lib/db";
+import Image from "next/image";
+import { notFound } from "next/navigation";
 
-const Modal = () => {
-  return <div>HI Modal</div>;
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+async function getProductsDetail(id: number) {
+  const product = await db.product.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      user: {
+        select: {
+          avatar: true,
+          username: true,
+        },
+      },
+    },
+  });
+
+  return product;
+}
+
+const Modal = async ({ params }: Props) => {
+  const product = await getProductsDetail(Number(params.id));
+  if (!product) {
+    return notFound();
+  }
+
+  return (
+    <div className="absolute w-full h-full z-50 left-0 top-0 flex justify-center items-center bg-black bg-opacity-60 ">
+      <Button uiType="modal">
+        <XMarkIcon className="size-10" />
+      </Button>
+      <div className="max-w-screen-sm flex flex-col justify-center w-full h-1/2">
+        <div className="relative aspect-square bg-neutral-700 rounded-md flex justify-center items-center text-neutral-200">
+          <Image
+            src={product.photo}
+            alt={product.title}
+            fill
+            className="object-cover"
+          />
+        </div>
+        <div className="flex gap-3 items-center p-5 border-b border-neutral-600">
+          <div className="size-10 rounded-full overflow-hidden">
+            {product.user.avatar !== null ? (
+              <Image
+                src={product.user.avatar}
+                width={40}
+                height={40}
+                alt={product.user.username}
+                className="object-cover"
+              />
+            ) : (
+              <UserIcon />
+            )}
+          </div>
+          <h3>{product.user.username}</h3>
+        </div>
+        <div className="flex flex-col gap-3 p-5">
+          <span className="text-2xl font-semibold">{product.title}</span>
+          <span>{product.description}</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
--- components/button.tsx
+++ components/button.tsx
@@ -1,22 +1,41 @@
 "use client";
-import React from "react";
+import React, { ReactNode } from "react";
 import { useFormStatus } from "react-dom";
+import { XMarkIcon } from "@heroicons/react/24/solid";
+import { useRouter } from "next/navigation";
 
 interface ButtonProps {
-  text: string;
+  uiType?: "page" | "modal";
+  text?: string;
+  children?: ReactNode;
 }
 
-const Button = ({ text }: ButtonProps) => {
+const Button = ({ text, uiType = "page" }: ButtonProps) => {
   const { pending } = useFormStatus();
+  const router = useRouter();
+  const onCloseClick = () => {
+    router.back();
+  };
+  const uiButton = {
+    page: (
+      <button
+        className="primary-btn h-10 disabled:bg-neutral-400 disabled:text-neutral-300 disabled:cursor-not-allowed"
+        disabled={pending}
+      >
+        {pending ? "Loading..." : text}
+      </button>
+    ),
+    modal: (
+      <button
+        className="absolute right-5 top-5 text-neutral-200"
+        onClick={onCloseClick}
+      >
+        <XMarkIcon className="size-10" />
+      </button>
+    ),
+  };
 
-  return (
-    <button
-      className="primary-btn h-10 disabled:bg-neutral-400 disabled:text-neutral-300 disabled:cursor-not-allowed"
-      disabled={pending}
-    >
-      {pending ? "Loading..." : text}
-    </button>
-  );
+  return <>{uiButton[uiType]}</>;
 };
 
 export default Button;
