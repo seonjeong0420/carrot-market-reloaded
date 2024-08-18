a777a68ed3b8305c2607258bb97972b2bc2ee13c
정선정 <seonjeong0420@sk.com>
2024-08-18 16:12:28 +0900

    feat: cache 갱신하는 방법1 - revalidate

--- app/(tabs)/home/page.tsx
+++ app/(tabs)/home/page.tsx
@@ -10,10 +10,15 @@ import React from "react";
 /**
  * 첫번째 argument : 비용이 많이 드는 계싼이나 데이터베이스 query를 가동시키는 함수
  * 두번째 argument = keyParts : 함수가 return하는 데이터를 cache 안에서 식별할 수 있게 해주는 것
+ * 세번째 argument : revalidate = 함수가 호출된 후 60초가 지나지 않은 경우에 캐시 안에 있는 데이터 return
  */
-const getCachedProducts = nextCache(getInitialProducts, ["home-products"]);
+const getCachedProducts = nextCache(getInitialProducts, ["home-products"], {
+  revalidate: 60,
+});
 
 async function getInitialProducts() {
+  console.log("test!!");
+
   await new Promise((resolve) => setTimeout(resolve, 2000));
   const products = await db.product.findMany({
     select: {
