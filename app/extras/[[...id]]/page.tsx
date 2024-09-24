5af3738cc4e6b2c6c020f2cb7954acca29524c0e
정선정_UI기술개발팀 <sunjeong0420@sk.com>
2024-09-24 15:36:08 +0900

    feat: #17 nextjs config logging

--- app/extras/[[...id]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const OptionalCatch = (props: Props) => {
-  return <div>optional Catch</div>;
-};
-
-export default OptionalCatch;
--- app/extras/page.tsx
+++ app/extras/page.tsx
@@ -1,13 +1,25 @@
+import { revalidatePath } from "next/cache";
 import React from "react";
 
-type Props = {};
+const getData = async () => {
+  const data = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies");
+};
 
-const Extras = (props: Props) => {
+const Extras = async () => {
+  await getData();
+  const action = async () => {
+    "use server";
+    revalidatePath("/extras");
+  };
+
   return (
     <div className="flex flex-col gap-3 py-10">
       <h1 className="text-6xl font-roboto">Extras Next.js Function</h1>
       <h2 className="font-rubik">so much more to learn!</h2>
       <span className="font-metallica text-4xl">metallica</span>
+      <form action={action}>
+        <button>revalidate</button>
+      </form>
     </div>
   );
 };
