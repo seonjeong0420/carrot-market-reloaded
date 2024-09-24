8185052a80baa674662980c83903b97f77bee540
정선정_UI기술개발팀 <sunjeong0420@sk.com>
2024-09-24 15:43:47 +0900

    feat: #17 Nextjs의 보안 관련 test code

---
+++ app/extras/hacked-component.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const HackedComponent = ({ data }: any) => {
+  return <div>HackedComponent</div>;
+};
+
+export default HackedComponent;
--- app/extras/page.tsx
+++ app/extras/page.tsx
@@ -1,12 +1,20 @@
 import { revalidatePath } from "next/cache";
-import React from "react";
+import React, { experimental_taintObjectReference } from "react";
+import HackedComponent from "./hacked-component";
 
 const getData = async () => {
   const data = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies");
+  const keys = {
+    apiKey: "11191119",
+    secret: "0022030",
+  };
+
+  experimental_taintObjectReference("API keys were leaked !! ", keys);
+  return keys;
 };
 
 const Extras = async () => {
-  await getData();
+  const data = await getData();
   const action = async () => {
     "use server";
     revalidatePath("/extras");
@@ -17,6 +25,8 @@ const Extras = async () => {
       <h1 className="text-6xl font-roboto">Extras Next.js Function</h1>
       <h2 className="font-rubik">so much more to learn!</h2>
       <span className="font-metallica text-4xl">metallica</span>
+      <HackedComponent data={data} />
+
       <form action={action}>
         <button>revalidate</button>
       </form>
